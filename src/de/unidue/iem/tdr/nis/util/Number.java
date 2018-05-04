7c42319ef2250de4ae6fd40160a410dcca8620c2
Evoxx <evoxx.general@gmail.com>
2018-05-04 12:55:15 +0200

    implemented some util stuff (number encodings) improved .gitignore

---
+++ src/de/unidue/iem/tdr/nis/util/Number.java
@@ -0,0 +1,20 @@
+package de.unidue.iem.tdr.nis.util;
+
+public class Number {
+
+    private final NumEncoding encoding;
+    private final String represenation;
+    private final int value;
+
+    public Number(NumEncoding encoding, String represenation) {
+        this.encoding = encoding;
+        this.represenation = represenation;
+        this.value = this.encoding.getValueOf(this.represenation);
+    }
+
+    public Number(NumEncoding encoding, int value) {
+        this.encoding = encoding;
+        this.value = value;
+        this.represenation = this.encoding.getRepresentationOf(this.value);
+    }
+}
